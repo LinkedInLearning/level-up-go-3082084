9395e60cf472537c2dd4627d9d3e5b782dff218e
addetz <adelinasimion@gmail.com>
2022-06-25 11:05:39 +0100

    add challenge 4 starting point

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+// coin contains the name and value of a coin
+type coin struct {
+	name  string
+	value float64
+}
+
+// coins is the list of values available for making change.
+var coins = []coin{
+	{name: "1 pound", value: 1},
+	{name: "50 pence", value: 0.50},
+	{name: "20 pence", value: 0.20},
+	{name: "10 pence", value: 0.10},
+	{name: "5 pence", value: 0.05},
+	{name: "1 penny", value: 0.01},
+}
+
+// calculateChange returns the coins required to calculate the
+func calculateChange(amount float64) map[coin]int {
+	panic("NOT IMPLEMENTED")
+}
+
+// printCoins prints all the coins in the slice to the terminal.
+func printCoins(change map[coin]int) {
+	if len(change) == 0 {
+		log.Println("No change found.")
+		return
+	}
+	log.Println("Change has been calculated.")
+	for coin, count := range change {
+		log.Printf("%d x %s \n", count, coin.name)
+	}
+}
+
+func main() {
+	amount := flag.Float64("amount", 0.0, "The amount you want to make change for")
+	flag.Parse()
+	change := calculateChange(*amount)
+	printCoins(change)
+}
