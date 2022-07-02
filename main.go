62392120683efecc49a03e81d2f9092107d9802d
addetz <adelinasimion@gmail.com>
2022-07-02 20:33:11 +0100

    add challenge 6 starting point

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
+
+// User represents a user record.
+type User struct {
+	Name    string `json:"name"`
+	Country string `json:"country"`
+}
+
+const path = "users.json"
+
+// getBiggestMarket takes in the slice of users and returns the biggest market.
+func getBiggestMarket(u []User) (string, int) {
+	panic("NOT IMPLEMENTED")
+}
+
+func main() {
+	users := importData()
+	country, count := getBiggestMarket(users)
+	log.Printf("The biggest user market is %s with %d users.\n", country, count)
+}
+
+// importData reads the raffle entries from file and creates the entries slice.
+func importData() []User {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var data []User
+	err = json.Unmarshal(file, &data)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return data
+}
