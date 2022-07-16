ba0d9fd682a8c472b9790ab57a5921c985fbb998
addetz <adelinasimion@gmail.com>
2022-07-16 11:34:48 +0100

    add challenge 10 solution

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"strconv"
 	"strings"
@@ -16,24 +17,46 @@ var operators = map[string]func(x, y float64) float64{
 }
 
 // parseOperand parses a string to a float64
-func parseOperand(op string) float64 {
-	parsedOp, _ := strconv.ParseFloat(op, 64)
-	return parsedOp
+func parseOperand(op string) (*float64, error) {
+	parsedOp, err := strconv.ParseFloat(op, 64)
+	if err != nil {
+		return nil, fmt.Errorf("cannot parse:%v", err)
+	}
+
+	return &parsedOp, nil
 }
 
 // calculate returns the result of a 2 operand mathematical expression eg "2 + 3"
-func calculate(expr string) float64 {
+func calculate(expr string) (*float64, error) {
 	ops := strings.Fields(expr)
-	left := parseOperand(ops[0])
-	right := parseOperand(ops[2])
-	f := operators[ops[1]]
-	result := f(left, right)
-	return result
+	nops := len(ops)
+	if nops != 3 {
+		return nil, fmt.Errorf("cannot calculate: need 3 ops, got %d", nops)
+	}
+	left, err := parseOperand(ops[0])
+	if err != nil {
+		return nil, err
+	}
+	right, err := parseOperand(ops[2])
+	if err != nil {
+		return nil, err
+	}
+	f, ok := operators[ops[1]]
+	if !ok {
+		return nil, fmt.Errorf("cannot calculate: %s is unknown operator", ops[1])
+	}
+
+	result := f(*left, *right)
+	return &result, nil
 }
 
 func main() {
 	expr := flag.String("expr", "", "The expression to calculate on, separated by spaces.")
 	flag.Parse()
-	result := calculate(*expr)
-	log.Printf("%s = %.2f\n", *expr, result)
+	result, err := calculate(*expr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Printf("%s = %.2f\n", *expr, *result)
 }
