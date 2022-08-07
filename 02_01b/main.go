0381f749e2add8e2e44f20ada19f51746f49113e
addetz <adelinasimion@gmail.com>
2022-08-07 11:41:55 +0100

    add challenge 11

---
+++ 02_01b/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"log"
+)
+
+var messages = []string{
+	"Hello!",
+	"How are you?",
+	"Are you just going to repeat what I say?",
+	"So immature",
+	"Stop copying me!",
+}
+
+// repeat concurrently prints out the given message n times
+func repeat(n int64, message string) {
+	panic("NOT IMPLEMENTED")
+}
+
+func main() {
+	factor := flag.Int64("factor", 0, "The fan-out factor to repeat by")
+	flag.Parse()
+	for _, m := range messages {
+		log.Println(m)
+		repeat(*factor, m)
+	}
+}
