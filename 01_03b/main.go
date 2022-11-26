d2cd515f92ff8643666b66eefbe171ff29600481
Derrick-Agozie <agozieokpara1@gmail.com>
2022-11-26 05:20:59 +0100

    solving exercise 3

--- 01_03b/main.go
+++ 01_03b/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"time"
@@ -10,12 +13,29 @@ const path = "entries.json"
 
 // raffleEntry is the struct we unmarshal raffle entries into
 type raffleEntry struct {
-	// TODO: Fill in definition
+	id   int
+	name string
 }
 
 // importData reads the raffle entries from file and creates the entries slice.
 func importData() []raffleEntry {
-	panic("NOT IMPLEMENTED")
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatal("Error:",err)
+	}
+	// fmt.Println(file)
+	strFile := string(file)
+	// println(strFile)
+
+	var entries []raffleEntry
+
+	error := json.Unmarshal([]byte(strFile), &entries)
+	if error != nil {
+		log.Fatal("Error occured:", error)
+	}
+	fmt.Println(entries)
+	return entries
+
 }
 
 // getWinner returns a random winner from a slice of raffle entries.
